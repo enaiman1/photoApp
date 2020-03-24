8799977687bbbe487c4edbe6225d75d3cc091afb
Ericcentrik <enaiman1@gmail.com>
2020-03-24 15:52:55 -0400

    got response back and am able to display image on page, user has a button to get a new random image

--- src/app/photo-fetch.service.ts
+++ src/app/photo-fetch.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoFetchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers:{
+        Authorization: 'Client-ID g-W2uKIvqBf5-RHfJzrKOA9CXW4KnDTuBIvzYnBc0ic' 
+      }
+    })
+  }
 }
--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PhotoFetchService } from '../photo-fetch.service'
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
   styleUrls: ['./photo-show.component.css']
 })
 export class PhotoShowComponent implements OnInit {
+  photoUrl: string;
 
-  constructor() { }
+  constructor(private photoService: PhotoFetchService) {
+    this.fetchPhoto()
+   }
+
+   onClick(){
+    this.fetchPhoto()
+   }
+
+   fetchPhoto(){
+    this.photoService.getPhoto().subscribe(response=>{
+      this.photoUrl = response.urls.regular;
+          });
+   }
 
   ngOnInit() {
   }
